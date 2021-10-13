59dbaf85031eda81787473e1768bf6adcc88250e
riko <andriidanylko12@gmail.com>
2021-10-13 21:38:56 +0300

    module-6-2

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,20 @@
     openModalBtn: document.querySelector('.header-btn'),
     closeModalBtn: document.querySelector('.close-btn'),
     modal: document.querySelector('.backdrop'),
+    modalItem: document.querySelector('.modal')
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.modal.addEventListener('click', removeModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+  }
+  function removeModal(e) {
+    if (e.target.value !== modalItem) {
+      refs.modal.classList.add('is-hidden');
+    }
+    
   }
 })();
